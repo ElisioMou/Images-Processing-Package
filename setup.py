fd245465404b78ebe4c8aa48b7a018781c331227
Elisio Moura <elisiomou@gmail.com>
2024-09-13 23:40:30 -0300

    Upload file setup.py.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup (
     description="Image Processing Package using Skimage",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/ElisioMou/Images-Processing-Package.git",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
